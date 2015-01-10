9aaf39a18a14ecd0cc06d6823b771fbf0496f6a0
James Ward <james@notjam.es>
2015-01-10 04:36:46 -0500

    add service for kaomoji resources

--- assets/scripts/services.js
+++ assets/scripts/services.js
@@ -8,5 +8,31 @@ kaomojiNinja.services = (function(angular) {
 		]
 	);
 
+	services.factory(
+		'Kaomoji',
+		[
+			'$resource',
+			function($resource){
+				return $resource(
+					'data/kaomoji.json',
+					{},
+					{
+						query: {
+							method: 'GET',
+							params: {},
+							cache: true,
+							isArray: true,
+							transformResponse: function(data, headers) {
+								var decodedData = angular.fromJson(data);
+
+								return decodedData;
+							}
+						}
+					}
+				);
+			}
+		]
+	);
+
 	return services;
 }(angular));
