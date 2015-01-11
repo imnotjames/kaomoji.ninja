a11a859195f1da3ea9a6da9abe1b355fdb3729a6
James Ward <james@notjam.es>
2015-01-11 00:10:20 -0500

    add pagination

--- assets/scripts/controllers.js
+++ assets/scripts/controllers.js
@@ -14,6 +14,38 @@ kaomojiNinja.controllers = (function(angular) {
 			$scope.kaomoji = function() {
 				return loadedKaomoji;
 			};
+
+			var page = 0;
+
+			$scope.paging = {
+				perPage: 16,
+
+				page: function() {
+					return page;
+				},
+
+				startFrom: function() {
+					return page * $scope.paging.perPage
+				},
+
+				count: function() {
+					return Math.ceil(
+						$scope.kaomoji().length / $scope.paging.perPage
+					);
+				},
+
+				next: function() {
+					if (page < $scope.paging.count() - 1) {
+						page += 1;
+					}
+				},
+
+				previous: function() {
+					if (page > 0) {
+						page -= 1;
+					}
+				}
+			};
 		}
 	]);
 
